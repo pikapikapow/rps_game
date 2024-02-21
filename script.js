c1bf804493cc2cfea067c5a39d5495ca61f852bb
pikapikapow <vladimirhwang84@gmail.com>
2024-02-21 18:39:45 -0500

    Started added the checking for the choices

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const btns = document.querySelectorAll(".btn");
 const userChoice = document.querySelector("#user-choice");
 const compChoice = document.querySelector("#computer-choice");
+const statusEl = document.querySelector(".status");
 
-console.log(compChoice);
+// console.log(compChoice);
 
 function whenClicked() {
   // console.log("clicked");
   // console.log(this.dataset.choice);
   userChoice.innerHTML = this.innerHTML;
+  userChoice.dataset.choice = this.dataset.choice;
   getRandom();
+  compChoices();
 }
 
 function getRandom() {
@@ -16,6 +19,15 @@ function getRandom() {
   // console.log(randNum);
   // console.log(btns[randNum]);
   compChoice.innerHTML = btns[randNum].innerHTML;
+  compChoice.dataset.choice = btns[randNum].dataset.choice;
+
+  // console.log(btns[randNum].dataset.choice);
 }
 
+function compChoices() {
+  if (compChoice.dataset.choice === userChoice.dataset.choice) {
+    console.log("You win");
+    statusEl.classList.add("show");
+  }
+}
 btns.forEach((btn) => btn.addEventListener("click", whenClicked));
