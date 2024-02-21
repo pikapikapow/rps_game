de23e30d4f7f90a5d733cf1f4f9a321722dc16a1
pikapikapow <vladimirhwang84@gmail.com>
2024-02-21 18:16:22 -0500

    Created randomization and picking for both sides

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const btns = document.querySelectorAll(".btn");
+const userChoice = document.querySelector("#user-choice");
+const compChoice = document.querySelector("#computer-choice");
+
+console.log(compChoice);
 
 function whenClicked() {
-  console.log("clicked");
-  console.log(this.dataset.choice);
+  // console.log("clicked");
+  // console.log(this.dataset.choice);
+  userChoice.innerHTML = this.innerHTML;
+  getRandom();
+}
+
+function getRandom() {
+  let randNum = Math.floor(Math.random() * btns.length);
+  // console.log(randNum);
+  // console.log(btns[randNum]);
+  compChoice.innerHTML = btns[randNum].innerHTML;
 }
 
 btns.forEach((btn) => btn.addEventListener("click", whenClicked));
