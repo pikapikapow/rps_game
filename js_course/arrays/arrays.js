f9e78acdb849a6b840dd69342bb3abc04da21b3f
pikapikapow <vladimirhwang84@gmail.com>
2024-04-18 18:46:13 -0400

    Todolist finished

--- js_course/arrays/arrays.js
+++ js_course/arrays/arrays.js
@@ -1,73 +1,53 @@
-const todoListEl = document.querySelector("#todo-list");
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2022-12-22",
+  },
+];
 
-const todoList = [];
+renderTodoList();
 
-function addTodo() {
-  const inputEl = document.querySelector(".js-input");
-  const name = inputEl.value;
-  todoList.unshift(name);
-  console.log(todoList);
-  todoListEl.innerHTML = "";
+function renderTodoList() {
+  let todoListHTML = "";
+
   for (let i = 0; i < todoList.length; i++) {
-    let listEl = document.createElement("li");
-    listEl.textContent = todoList[i];
-    todoListEl.appendChild(listEl);
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-todo-button">Delete</button> 
+    `;
+    todoListHTML += html;
   }
 
-  //   let listEl = document.createElement("li");
-  //   listEl.textContent = name;
-  //   todoListEl.appendChild(listEl);
-  inputEl.value = "";
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
 
-// for (let i = 0; i < todoList.length; i++) {
-//   let listEl = document.createElement("li");
-//   listEl.textContent = todoList[i];
-//   todoListEl.appendChild(listEl);
-// }
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
 
-// let i = 1;
+  const dateInputElement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputElement.value;
 
-// while (i <= 5) {
-//   console.log(i);
-//   i++;
-// }
+  todoList.push({
+    //name: name,
+    //dueDate: dueDate,
+    name,
+    dueDate,
+  });
 
-// for (let j = 1; j <= 5; j++) {
-//   console.log(j);
-// }
+  inputElement.value = "";
 
-// const bucketList = [
-//   "Finish writing report",
-//   "Buy groceries",
-//   "Call mom",
-//   "Go for a run",
-//   "Schedule dentist appointment",
-//   "Pay bills",
-//   "Clean the house",
-//   "Read a chapter of a book",
-//   "Organize desk",
-//   "Plan weekend activities",
-// ];
-
-// for (let i = 0; i < bucketList.length; i++) {
-//   console.log(bucketList[i]);
-// }
-
-// const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
-
-// let total = 0;
-
-// for (let i = 0; i < numbers.length; i++) {
-//   total += numbers[i];
-// }
-
-// console.log(total);
-
-// let newArr = [];
-
-// for (let i = 0; i < numbers.length; i++) {
-//   newArr.push(numbers[i] * 2);
-// }
-
-// console.log(newArr);
+  renderTodoList();
+}
