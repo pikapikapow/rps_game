c7b791e14f2d4cbedf70c9db753314a1c164f1cb
pikapikapow <vladimirhwang84@gmail.com>
2024-04-28 13:45:45 -0400

    Cleaned up explorer, continuing on with rps autoplay feature

--- js_course/advanced_functions/adfunc.js
+++ js_course/advanced_functions/adfunc.js
@@ -1,40 +1,61 @@
-const buttonElement = document.querySelector(".js-button");
+// const buttonElement = document.querySelector(".js-button");
 
-const eventListener = () => {
-  console.log("click");
-};
-buttonElement.addEventListener("click", eventListener);
+// const eventListener = () => {
+//   console.log("click");
+// };
+// buttonElement.addEventListener("click", eventListener);
 
-buttonElement.removeEventListener("click", eventListener);
+// buttonElement.removeEventListener("click", eventListener);
 
-buttonElement.addEventListener("click", () => {
-  console.log("click2");
-});
+// buttonElement.addEventListener("click", () => {
+//   console.log("click2");
+// });
 
-greeting();
-function greeting() {
-  console.log("hello");
-}
-greeting();
+// greeting();
+// function greeting() {
+//   console.log("hello");
+// }
+// greeting();
 
-const num = 2;
-const function1 = function () {
-  console.log("hello2");
-};
+// const num = 2;
+// const function1 = function () {
+//   console.log("hello2");
+// };
 
-console.log(function1);
-console.log(typeof function1);
-function1();
+// console.log(function1);
+// console.log(typeof function1);
+// function1();
 
-const object1 = {
-  num: 2,
-  fun: function () {
-    console.log("hello3");
-  },
-};
-object1.fun();
+// const object1 = {
+//   num: 2,
+//   fun: function () {
+//     console.log("hello3");
+//   },
+// };
+// object1.fun();
 
-function display(param) {
-  console.log(param);
-}
-display(2);
+// function display(param) {
+//   console.log(param);
+// }
+// display(2);
+
+// function run(param) {
+//   param();
+// }
+
+// run(function () {
+//   console.log("hello4");
+// });
+
+setTimeout(function () {
+  console.log("timeout");
+  console.log("timeout2");
+}, 3000);
+
+console.log("next line");
+
+setInterval(function () {
+  console.log("interval");
+}, 3000);
+
+console.log("next linee");
